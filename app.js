3225f20939555441d6ba6e818d5075501f60d942
IrshadAli111 <irshad.ali1@undp.org>
2024-10-24 13:07:37 +0530

    Code cleaning
    Code cleaning

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const port = 3000;
 
 
 const openai = new OpenAI({
-  //organization: process.env.organization,
-  //project:process.env.project,
+  organization: process.env.organization,
+  project:process.env.project,
 });
 
 app.use(bodyParser.json());
@@ -33,7 +33,7 @@ app.post('/chat', async (req, res) => {
 
     // Call OpenAI API
     const response = await openai.chat.completions.create({
-      model: 'ft:gpt-4o-mini-2024-07-18:personal:pmfby:ALKxWo0M',  // Choose the GPT model
+      model: process.env.Model_Name,  // Choose the GPT model
       messages: [
         { role: 'system', content: "You are an expert in Crop Insurance. Only respond as per trained data and to questions related to this field including topics like post-harvest losses, crop management, etc. If the question is not relevant with Crop Insurance then respond with: 'I can only assist with PMFBY Scheme & crop insurance-related inquiries'." },
         { role: 'user', content: prompt },
@@ -69,7 +69,7 @@ try {
 
   // Call OpenAI API
   const response = await openai.chat.completions.create({
-    model: 'gpt-4o-mini-2024-07-18',  // Choose the GPT model
+    model: process.env.Model_Name,  // Choose the GPT model
     messages: [
       { role: 'system', content: "You are an assistant that understands intents for crop insurance queries. Extract the words : Claim Status,Policy Status,Insurance Policy,Ticket Status & Crop Loss Intimation Status and also Extract the season (like 'Kharif', 'Rabi') and year (like 2024) from the input as well. I need output in format with proper json object format for example {\"res\": \"Claim Status - Rabi 2024\",\"message\":\"\"},{\"res\": \"Claim Status -\",\"message\":\"Year not specified\"} ,{\"res\": \"- Rabi 2023\",\"message\":\"Do you mean Claim Status/Policy Status/Ticket Status/Insurance Policy/Crop Loss Initimation Status\"} etc." },
       { role: 'user', content: `${prompt}` },
@@ -127,7 +127,7 @@ app.post('/train_by_data', async (req, res) => {
 
     // Call OpenAI API
     const response = await openai.chat.completions.create({
-      model:'file-PrEdENKoHZnzt0VcujLh4yp7', //'gpt-3.5-turbo',  // Choose the GPT model
+      model:process.env.Model_Name, 
       messages: [
         { role: 'system', content: 'You are pmfby chatbot and answers only on trained data.' },
         { role: 'user', content: prompt },
@@ -151,42 +151,7 @@ app.post('/train', async (req, res) => {
     //console.log("Env Key",process.env.OPENAI_API_KEY);
   
   try {
-    // Call OpenAI API
-//     const response = await openai.chat.completions.create({
-//       model: 'gpt-3.5-turbo',  // Choose the GPT model
-//       messages: [
-//         { role: 'system', content: 'You are a helpful pmfby chatbot.' },
-//         { role: 'user', content: `
-//   You are a helpful assistant. Hey Please add the below question and answers and train yourself 
-
-//   """
-//     Question: What is Pradhan Mantri Fasal Bima Yojana (PMFBY)?
-//     Answer: Pradhan Mantri Fasal Bima Yojana (PMFBY) is a Government Scheme that provides crop insurance protection to farmers when notified crops are damaged due to natural calamities like hailstorm, drought, floods, cyclones, heavy and unseasonal rains, etc.
-
-//     Question: When was the scheme launched?
-//     Answer: The PMFBY scheme was launched on 18th February 2016 by the honorable Prime Minister of India.
-
-//     Question: From which date has the revamped PMFBY scheme come into effect?
-//     Answer: The Scheme was revamped in February 2020 and made voluntary for all farmers for participation.
-  
-//     Question: What is crop insurance?
-//     Answer: Crop insurance is an important risk mitigation tool to protect financial losses of farmers from unforeseen natural calamities like hailstorm, drought, floods, cyclones, heavy and unseasonal rains, attack of disease and pests etc.
-  
-//     Question: Why farmers should avail crop insurance?
-//     Answer: An adverse weather/climatic conditions can cause significant damage to standing crops such as heavy rains, floods, hailstones, cloudbursts, landslide and natural fire etc, causing damage to crop partially or wholly. It impacts our hard-working farmers severely, burdening them economically and socially. Therefore, it is imperative to provide economic and social security to the farmers so that they are protected from probable crop loss with the help of crop insurance."""
-    
-//     Question: What are the provisions under PMFBY/RWBCIS to help farmers?
-//     Answer: Pradhan Mantri Fasal Bima Yojana (PMFBY) launched in 2016, aims at providing crop insurance protection to farmers, when notified crops are damaged due to unexpected natural calamities. Notified crops are the crops identified by the State Govts to be covered under crop insurance. Field crops like cereals, pulses and oilseeds are covered under PMFBY while horticulture crops mainly fruit, and vegetable crops are covered under Restructured Weather Based Crop Insurance Scheme (RWBCIS). The scheme offers very low premium rates for farmers to participate in the scheme.
-
-
-//     Please answer the below as per trained data going forward.
-
-//   ` },
-//       ],
-//       temperature :0.2
-//     });
-
-    // If you have access to Node fs we recommend using fs.createReadStream():
+ 
     const response = await openai.files.create({ file: fs.createReadStream('training.jsonl'), purpose: 'fine-tune' });
 
     console.log("Response",response)
